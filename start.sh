e769c4c1a034158f526283874e790c3cb7c04041
0xGingi <0xgingi@0xgingi.com>
2024-12-26 16:37:24 +0000

    fix start/build script

--- start.sh
+++ start.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 echo "Stopping all containers..."
-docker compose down
+docker compose --profile all down
 
 echo "Cleaning up Docker system..."
 docker system prune -f
 docker volume prune -f
 
 echo "Removing old images..."
-docker compose rm -f
+docker compose --profile all rm -f
 
 echo "Building new images..."
-docker compose build --no-cache
+docker compose --profile all build --no-cache
 
 echo "Starting all services..."
 docker compose --profile all up -d
 
 echo "Showing logs..."
-docker compose logs -f
+docker compose --profile all logs -f
